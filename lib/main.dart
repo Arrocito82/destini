2ff7e0751ee79822287e510e4a6ee96ab01aa7ba
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-12 12:10:25 -0600

    steps 22 to 29: show only restart button if story ends

--- lib/main.dart
+++ lib/main.dart
@@ -74,31 +74,32 @@ class _StoryPageState extends State<StoryPage> {
               SizedBox(
                 height: 20.0,
               ),
-              Expanded(
-                flex: 2,
-                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: TextButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    setState(() {
-                      storyBrain.nextStory(storyBrain.CHOICE2);
-                    });
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateColor.resolveWith(
-                        (Set<MaterialState> states) {
-                      if (states.any((MaterialState currentState) =>
-                          currentState == MaterialState.pressed)) {
-                        return Colors.blue.shade800;
-                      } else {
-                        return Colors.blue;
-                      }
-                    }),
-                  ),
-                  child: Text(
-                    '${storyBrain.getChoice2()}',
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+              Visibility(
+                visible: storyBrain.buttonShouldBeVisible(),
+                child: Expanded(
+                  flex: 2,
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        storyBrain.nextStory(storyBrain.CHOICE2);
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateColor.resolveWith(
+                          (Set<MaterialState> states) {
+                        if (states.any((MaterialState currentState) =>
+                            currentState == MaterialState.pressed)) {
+                          return Colors.blue.shade800;
+                        } else {
+                          return Colors.blue;
+                        }
+                      }),
+                    ),
+                    child: Text(
+                      '${storyBrain.getChoice2()}',
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
                   ),
                 ),
               ),
@@ -109,7 +110,3 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
