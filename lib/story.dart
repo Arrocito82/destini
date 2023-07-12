95100bd543d84455606e8894de792fa3a39c683e
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-12 11:28:00 -0600

    steps 1 to 8: create StoryBrain Class

--- lib/story.dart
+++ lib/story.dart
@@ -1,7 +1,18 @@
 class Story {
-  String _storyTitle;
-  String _choice1;
-  String _choice2;
+  String _storyTitle = '';
+  String _choice1 = '';
+  String _choice2 = '';
 
-  Story(this._storyTitle, this._choice1, this._choice2);
+  String getStoryTitle() => this._storyTitle;
+  String getChoice1() => this._choice1;
+  String getChoice2() => this._choice2;
+
+  Story(
+      {required String storyTitle,
+      required String choice1,
+      required String choice2}) {
+    _storyTitle = storyTitle;
+    _choice1 = choice1;
+    _choice2 = choice2;
+  }
 }
